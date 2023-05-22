b8e4b600dd36860977dfedbd7138cdaad502766c
Gabriel Pacini <Gabriu10@gmail.com>
2023-05-22 14:32:32 -0300

    First Commit

---
+++ Game-Jam/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] private float velocity;
+
+    private Vector2 targetPosition;
+
+    public Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (rb.position != targetPosition)
+        {
+            Vector2 position = Vector2.MoveTowards(rb.position, targetPosition, velocity * Time.fixedDeltaTime);
+            rb.MovePosition(position);
+        }
+    }
+}
